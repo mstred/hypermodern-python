a00de53b534c8ade5fff6cf824556dc36dcfbeba
Edson Samuel Jr <samuedson@gmail.com>
2020-12-20 16:18:42 -0300

    feat: prepare base structure for tests and install related deps

---
+++ noxfile.py
@@ -0,0 +1,8 @@
+from nox import session as nox_session
+
+
+@nox_session()
+def tests(session):
+    session.run("poetry", "install", external=True)
+    session.run("pytest", *(session.posargs or ["--cov"]))
+
